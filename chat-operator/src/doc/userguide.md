e45a02fd9edfa71b0717e0c9f28149a45ace36e7
Francisco de la Vega <fdelavega@ficodes.com>
2019-12-03 08:27:25 +0100

    Add Chat operator template

---
+++ chat-operator/src/doc/userguide.md
@@ -0,0 +1,17 @@
+## Introduction
+
+Chat Operator for Bootcamp
+
+## Settings
+
+## Wiring
+
+### Input Endpoints
+
+### Output Endpoints
+
+## Usage
+
+## Reference
+
+- [FIWARE Mashup](https://mashup.lab.fiware.org/)
---
+++ chat-operator/src/js/main.js
@@ -0,0 +1,21 @@
+/*
+ * chat-operator
+ * https://github.com/fdelavega/bootcamp.materials
+ *
+ * Copyright (c) 2019 FICODES
+ * Licensed under the MIT license.
+ */
+
+(function () {
+
+    "use strict";
+
+    MashupPlatform.prefs.registerCallback(function (new_preferences) {
+
+    }.bind(this));
+
+    /* test-code */
+
+    /* end-test-code */
+
+})();
